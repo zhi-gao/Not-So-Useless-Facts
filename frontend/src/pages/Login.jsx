3b71625e38f012c1ee61f7274ce2160ea894381e
ccny-ChenhaoLi <cli005@citymail.cuny.edu>
2023-11-11 14:33:24 -0500

    login css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,20 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom"
+import styles from "./Login.module.css"
 
 export default function Login() {
     const navigate = useNavigate();
 
-    const usernameRef = useRef();
+    const emailRef = useRef();
     const passwordRef = useRef();
 
     async function submitHandler(e) {
         e.preventDefault();
 
-        const username = usernameRef.current?.value || "";
+        const email = emailRef.current?.value || "";
         const password = passwordRef.current?.value || "";
 
-        if(!username) {
+        if(!email) {
 
         }
 
@@ -24,18 +25,24 @@ export default function Login() {
         // make request
     }
 
-    return <div>
-        <h1>Login</h1>
+    return <div className={styles.flexContainer}>
 
-        <form onSubmit={submitHandler}>
-            <label>Username</label><br></br>
-            <input ref={usernameRef} /><br></br>
+        <form onSubmit={submitHandler} className={styles.login}>
+            <h1>Login</h1>
+            <div className={styles.inputContainer}>
+                <label>Email:</label>
+                <input ref={emailRef} /><br></br>
+            </div>
 
-            <label>Password</label><br></br>
-            <input ref={passwordRef} /><br></br>
+            <div className={styles.inputContainer}>
+                <label>Password:</label>
+                <input ref={passwordRef} type="password" /><br></br>
+            </div>
 
-            <a onClick={() => navigate("/register")}>New User? Register</a><br></br>
-            <button>Login</button>
+            <div id={styles.centerContainer}>
+                <a onClick={() => navigate("/register")}>New User? Register</a><br></br>
+                <button>Login</button>
+            </div>
         </form>
     </div>
 }
