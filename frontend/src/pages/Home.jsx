cde1c40093a9055ad706ee17c01805a9be26c7a4
ccny-ChenhaoLi <cli005@citymail.cuny.edu>
2023-11-28 13:57:43 -0500

    login btn change to profile btn if logged in

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,38 @@
 import { useNavigate } from "react-router-dom"
 import Navbar from "../components/Nabar";
 import styles from "./Home.module.css"
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
+import { authRequest } from "../requests/authRequest";
 
 export default function Home() {
     const navigate = useNavigate();
     const {currentUser, setCurrentUser} = useContext(UserContext);
-    
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const auth = async () => {
+            if(!localStorage.getItem("token")) {
+                return;
+            }
+
+            if(JSON.stringify(currentUser) === "{}") {
+                try {
+                    // make auth request
+                    const data = await authRequest();
+                    setCurrentUser(data);
+                    setIsUserLoggedIn(true);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        }
+        auth();
+    }, []);
+
     return <div>
-        <Navbar primaryButton={JSON.stringify(currentUser) === "{}" ? "Login" : "Profile"} primaryButtonOnClick={JSON.stringify(currentUser) === "{}" ? () => navigate("/login") : () => navigate("/profile")} 
-                secondaryButton="Past Facts" secondaryButtonOnClick={() => navigate("/all-facts")}
-                thirdButton="About Us" thirdButtonOnClick={() => navigate("/about")} />
+        {!isUserLoggedIn ? <Navbar primaryButton="Login" primaryButtonOnClick={() => navigate("/login")} secondaryButton="Past Facts" secondaryButtonOnClick={() => navigate("/all-facts")}thirdButton="About Us" thirdButtonOnClick={() => navigate("/about")} /> :  <Navbar primaryButton="Profile" primaryButtonOnClick={() => navigate("/profile")} secondaryButton="Past Facts" secondaryButtonOnClick={() => navigate("/all-facts")}thirdButton="About Us" thirdButtonOnClick={() => navigate("/about")} />}
+        
 
         <div className={styles.flexContainer}>
             <div id={styles.fotd}>
